5735d285aa12c7511c2f5def1bc231b6f9e7cda9
Edwin Del Bosque <edwindelbosquer@gmail.com>
2019-08-21 19:38:55 -0600

    Add CSS styling and class for Hydration testing
    Co-Authored By: Vanessa Randall @vrandall66

--- src/HydrationRepository.js
+++ src/HydrationRepository.js
@@ -1,6 +1,7 @@
 class HydrationRepository {
-  constructor(data) {
+  constructor(data, id) {
     this.data = data;
+    this.userHydro = this.getHydrationData(id);
   }
 
   getHydrationData(id) {
@@ -14,14 +15,35 @@ class HydrationRepository {
     }, 0) / this.getHydrationData(id).length);
   }
 
-  totalOzDay(day) {
-    return this.data.find(user => user.date === day).numOunces;
+  getCurrentDate() {
+    let today = new Date();
+    let dd = today.getDate();
+    let mm = today.getMonth() + 1;
+    let yyyy = today.getFullYear();
+
+    if (dd < 10) {
+      dd = '0' + dd
+    }
+
+    if (mm < 10) {
+      mm = '0' + mm
+    }
+
+    today = `${yyyy}/${mm}/${dd}`;
+    console.log(today);
+    return today;
   }
 
-  weeklyHydrationAvg(id, day) {
-    const userData = this.getHydrationData(id);
-    const indexLastDay = userData.findIndex(data => data.date === day);
-    const lastWeekData = userData.splice(indexLastDay - 6, indexLastDay);
+  totalOzDay() {
+    return this.userHydro.find(user => user.date === this.getCurrentDate()).numOunces;
+  }
+
+  weeklyHydrationAvg() {
+    const userData = this.userHydro;
+    const indexCurrentDay = userData.findIndex(data => data.date === this.getCurrentDate());
+    const lastWeekData = userData.splice(indexCurrentDay - 6, indexCurrentDay);
+    console.log(indexCurrentDay);
+    console.log(lastWeekData);
     return lastWeekData;
   }
 }
