e11fb1c97f02d424e430c546d6dce8c4a6d31d58
Vanessa Randall <vanessalrandall@gmail.com>
2019-08-24 20:15:20 -0600

    Finish testing and DOM manipulation for Sleep Repository
    Co-authored-by: Edwin Del Bosque @edwindelbosque

--- src/scripts.js
+++ src/scripts.js
@@ -6,6 +6,10 @@ const dailyStepGoal = $('#stepGoal');
 const stepCompare = $('#step-compare');
 const dailyOz = $('#daily-oz');
 const weeklyOz = $('#weekly-oz');
+const date = $('#date');
+const yesterdaySleep = $('#yesterday-sleep');
+const weeklySleep = $('#weekly-sleep');
+const allTimeSleep = $('#all-time-sleep');
 
 let userIdRandomizer = Math.floor(Math.random() * (50 - 1) + 1);
 let userRepository = new UserRepository(userData);
@@ -19,6 +23,8 @@ $(document).ready(() => {
   displayDailyOz();
   displayWeeklyOz();
   sleepRepository.getBestSleepers();
+  displayCurrentDate();
+  displaySleep();
 });
 
 function updateUserDataDOM(userInfo) {
@@ -30,7 +36,7 @@ function updateUserDataDOM(userInfo) {
 }
 
 function compareStepGoal(userInfo) {
-  const avgStep = userRepository.getAvgStep()
+  const avgStep = userRepository.getAvgStep();
   const dailyStep = userInfo.dailyStepGoal;
   const numSteps = Math.abs(avgStep - dailyStep);
   const stepStatus = avgStep > dailyStep ? `under` : `over`;
@@ -42,9 +48,9 @@ function displayDailyOz() {
 }
 
 function displayWeeklyOz() {
-  const users = hydrationRepository.weeklyHydrationAvg(getCurrentDate())
-  return users.forEach(user => {
-    return $(`<li>${user.date}: ${user.numOunces} oz</li>`).appendTo(weeklyOz)
+  const users = hydrationRepository.weeklyHydrationAvg(getCurrentDate());
+  return users.forEach(log => {
+    return $(`<li>${log.date}: ${log.numOunces} oz</li>`).appendTo(weeklyOz);
   });
 }
 
@@ -64,4 +70,40 @@ function getCurrentDate() {
 
   today = `${yyyy}/${mm}/${dd}`;
   return today;
+}
+
+function getCurrentDateDOM() {
+  let today = new Date();
+  let dd = today.getDate();
+  let mm = today.getMonth() + 1;
+  let yyyy = today.getFullYear();
+
+  if (dd < 10) {
+    dd = '0' + dd;
+  }
+
+  if (mm < 10) {
+    mm = '0' + mm;
+  }
+
+  today = `Date: ${mm}/${dd}/${yyyy}`;
+  return today;
+}
+
+function displayCurrentDate() {
+  date.text(getCurrentDateDOM());
+}
+
+function displaySleep() {
+  const userLogsHours = sleepRepository.getAllTimeAvg();
+  const userLogsQuality = sleepRepository.getQualitySleepAvg();
+  const weeklyData = sleepRepository.weeklySleepData(getCurrentDate());
+
+  $(`<p>You slept ${sleepRepository.getDailySleepHours(getCurrentDate())} hours last night!</p>`).appendTo(yesterdaySleep);
+  $(`<li>Hours Slept: ${userLogsHours}</li>`).appendTo(allTimeSleep);
+  $(`<li>Quality of sleep: ${userLogsQuality}</li>`).appendTo(allTimeSleep);
+
+  weeklyData.forEach(day => {
+    return $(`<li>${day.date}: ${day.hoursSlept} hours, ${day.sleepQuality} qual</li>`).appendTo(weeklySleep);
+  });
 }
