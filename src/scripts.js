5eb83390e534269c5240ee3ff74082b751a1d8d7
Edwin Del Bosque <edwindelbosquer@gmail.com>
2019-08-25 20:17:29 -0600

    Add additional TDD to ActivityRepo class and DOM manipulation
    Co-Authored By: Vanessa Randall @vrandall66

--- src/scripts.js
+++ src/scripts.js
@@ -6,19 +6,32 @@ const dailyStepGoal = $('#stepGoal');
 const stepCompare = $('#step-compare');
 const dailyOz = $('#daily-oz');
 const weeklyOz = $('#weekly-oz');
+const date = $('#date');
+const yesterdaySleep = $('#yesterday-sleep');
+const weeklySleep = $('#weekly-sleep');
+const allTimeSleep = $('#all-time-sleep');
+const dailySteps = $('#daily-steps');
+const dailyDistance = $('#daily-distance');
+const dailyMinutes = $('#daily-minutes');
+const compareActivity = $('#compare-activity');
+const weeklyActivity = $('#weekly-activity');
 
 let userIdRandomizer = Math.floor(Math.random() * (50 - 1) + 1);
-let userRepository = new UserRepository(userData);
+let userRepository = new UserRepository(userData, userIdRandomizer);
 let hydrationRepository = new HydrationRepository(hydrationData, userIdRandomizer);
 let sleepRepository = new SleepRepository(sleepData, userIdRandomizer);
+let activityRepository = new ActivityRepository(userIdRandomizer, activityData);
 
 $(document).ready(() => {
-  let userInfo = userRepository.getUserData(userIdRandomizer);
+  let userInfo = userRepository.getUserData();
   updateUserDataDOM(userInfo);
   compareStepGoal(userInfo);
   displayDailyOz();
   displayWeeklyOz();
   sleepRepository.getBestSleepers();
+  displayCurrentDate();
+  displaySleep();
+  displayActivity()
 });
 
 function updateUserDataDOM(userInfo) {
@@ -30,7 +43,7 @@ function updateUserDataDOM(userInfo) {
 }
 
 function compareStepGoal(userInfo) {
-  const avgStep = userRepository.getAvgStep()
+  const avgStep = userRepository.getAvgStep();
   const dailyStep = userInfo.dailyStepGoal;
   const numSteps = Math.abs(avgStep - dailyStep);
   const stepStatus = avgStep > dailyStep ? `under` : `over`;
@@ -42,9 +55,9 @@ function displayDailyOz() {
 }
 
 function displayWeeklyOz() {
-  const users = hydrationRepository.weeklyHydrationAvg(getCurrentDate())
-  return users.forEach(user => {
-    return $(`<li>${user.date}: ${user.numOunces} oz</li>`).appendTo(weeklyOz)
+  const users = hydrationRepository.weeklyHydrationAvg(getCurrentDate());
+  return users.forEach(log => {
+    return $(`<li>${log.date}: ${log.numOunces} oz</li>`).appendTo(weeklyOz);
   });
 }
 
@@ -64,4 +77,56 @@ function getCurrentDate() {
 
   today = `${yyyy}/${mm}/${dd}`;
   return today;
+}
+
+function getCurrentDateDOM() {
+  let today = new Date();
+  let dd = today.getDate();
+  let mm = today.getMonth() + 1;
+  let yyyy = today.getFullYear();
+
+  if (dd < 10) {
+    dd = '0' + dd;
+  }
+
+  if (mm < 10) {
+    mm = '0' + mm;
+  }
+
+  today = `Date: ${mm}/${dd}/${yyyy}`;
+  return today;
+}
+
+function displayCurrentDate() {
+  date.text(getCurrentDateDOM());
+}
+
+function displaySleep() {
+  const userLogsHours = sleepRepository.getAllTimeAvg();
+  const userLogsQuality = sleepRepository.getQualitySleepAvg();
+  const weeklyData = sleepRepository.weeklySleepData(getCurrentDate());
+
+  $(`<p>You slept ${sleepRepository.getDailySleepHours(getCurrentDate())} hours last night!</p>`).appendTo(yesterdaySleep);
+  $(`<li>Hours Slept: ${userLogsHours}</li>`).appendTo(allTimeSleep);
+  $(`<li>Quality of sleep: ${userLogsQuality}</li>`).appendTo(allTimeSleep);
+
+  weeklyData.forEach(day => {
+    return $(`<li>${day.date}: ${day.hoursSlept} hours, ${day.sleepQuality} qual</li>`).appendTo(weeklySleep);
+  });
+}
+
+function displayActivity() {
+  $(`<p>You took ${activityRepository.getDailyStats(getCurrentDate(), 'numSteps')} steps today!</p>`).appendTo(dailySteps);
+  $(`<p>You were active for ${activityRepository.getMinutesActive(getCurrentDate())} minutes today!</p>`).appendTo(dailyDistance);
+  $(`<p>You walked ${activityRepository.getMilesWalked(getCurrentDate(), userRepository.getUserData())} miles today!</p>`).appendTo(dailyMinutes);
+
+  $(`<li>${activityRepository.getAverageStairsDay(getCurrentDate()) - activityRepository.getDailyStats(getCurrentDate(), 'flightsOfStairs')} stairs from the average</li>`).appendTo(compareActivity);
+  $(`<li>${activityRepository.getAverageStepsDay(getCurrentDate()) - activityRepository.getDailyStats(getCurrentDate(), 'numSteps')}  from the average</li>`).appendTo(compareActivity);
+  $(`<li>${activityRepository.getAvergageMinutesActive(getCurrentDate()) - activityRepository.getDailyStats(getCurrentDate(), 'minutesActive')} from the average</li>`).appendTo(compareActivity);
+
+
+  // $(`<p>You took ${activityRepository.getDailySteps(getCurrentDate())} steps today!</p>`).appendTo(weeklyActivity);
+  // $(`<p>You took ${activityRepository.getDailySteps(getCurrentDate())} steps today!</p>`).appendTo(weeklyActivity);
+  // $(`<p>You took ${activityRepository.getDailySteps(getCurrentDate())} steps today!</p>`).appendTo(weeklyActivity);
+
 }
--- test/Activity-test.js
+++ test/Activity-test.js
@@ -1,11 +1,12 @@
 const chai = require('chai');
 const expect = chai.expect;
-const ActivityRepository = require('../src/ActivityRepository')
+const ActivityRepository = require('../src/ActivityRepository');
+const User = require('../src/User.js');
 
-let activityRepository, activityData;
+let activityRepository, activityData, user, sampleData;
 
 beforeEach(() => {
-  const activityData =
+  activityData =
     [
       { "userID": 13, "date": "2019/08/18", "numSteps": 1242, "minutesActive": 64, "flightsOfStairs": 16 },
       { "userID": 13, "date": "2019/08/19", "numSteps": 3425, "minutesActive": 112, "flightsOfStairs": 4 },
@@ -21,6 +22,17 @@ beforeEach(() => {
       { "userID": 5, "date": "2019/08/25", "numSteps": 842, "minutesActive": 43, "flightsOfStairs": 86 },
     ];
 
+  sampleData = {
+    "id": 13,
+    "name": "Tom Schmeler",
+    "address": "1524 Clemmie River, Newtonbury RI 02849-3159",
+    "email": "Leopoldo.Sauer@gmail.com",
+    "strideLength": 3.2,
+    "dailyStepGoal": 4000,
+    "friends": [33, 14, 3, 43, 35]
+  };
+
+  user = new User(sampleData);
   activityRepository = new ActivityRepository(13, activityData);
 });
 
@@ -32,6 +44,57 @@ describe('ActivityRepository', () => {
 
   it('should be an instance of ActivityRepository', () => {
     expect(activityRepository).to.be.an.instanceOf(ActivityRepository);
-  })
+  });
+
+  it('should return the miles a user has walked based on their number of steps and stride length', () => {
+    expect(activityRepository.getMilesWalked('2019/08/25', user)).to.equal(5.7); //5.66787878.......
+  });
+
+  it('should return how many minutes the user was active for a given day', () => {
+    expect(activityRepository.getMinutesActive('2019/08/24')).to.equal(456);
+  });
+
+  it('should return how many minutes active the user averaged for the week', () => {
+    expect(activityRepository.getAverageActivity('2019/08/24')).to.equal(370); //369.714286
+  });
+
+  it('should return whether a user met their daily step goal for a given date', () => {
+    expect(activityRepository.reachedDailyStepGoal('2019/08/24', sampleData)).to.equal(false);
+  });
+
+  it('should return the days where the user exceeded their step goal', () => {
+    expect(activityRepository.getAllTimeExceededSteps(sampleData)).to.deep.equal(
+      [
+        { "date": "2019/08/20", "numSteps": 5321 },
+        { "date": "2019/08/23", "numSteps": 5321 },
+        { "date": "2019/08/25", "numSteps": 9352 },
+      ]);
+  });
+
+  it('should return the user"s all-time stair climbing record', () => {
+    expect(activityRepository.getAllTimeStairClimb()).to.deep.equal(
+      { "date": "2019/08/20", "flightsOfStairs": 174 }
+    );
+  });
+
+  it('should return the average number of stairs climbed for all users on a given date', () => {
+    expect(activityRepository.getAverageStairsDay('2019/08/25')).to.equal(90);
+  });
+
+  it('should return the average number of steps taken for all users on a given date', () => {
+    expect(activityRepository.getAverageStepsDay('2019/08/25')).to.equal(5540);
+  });
+
+  it('should return the average number of minutes active for all users on a given date', () => {
+    expect(activityRepository.getAvergageMinutesActive('2019/08/25')).to.equal(226);
+  });
+
+  it('should return the number of kilometers a user walked in a given date', () => {
+    expect(activityRepository.getKilometersWalked('2019/08/25', user)).to.equal(9.2);
+  });
+
+  it('should return the number of daily steps', () => {
+    expect(activityRepository.getDailyStats('2019/08/25', 'numSteps')).to.equal(9352);
+  });
 
 });
